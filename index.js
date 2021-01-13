59b8548025a755e2a280edc69b8330afba1b3d9a
handod <69374221+handod@users.noreply.github.com>
2021-01-13 20:24:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function rollFunction() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource1 = "images/" + "dice" + randomNumber1 + ".png";
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomImageSource1);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource2 = "images/" + "dice" + randomNumber2 + ".png";
+  var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomImageSource2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩"
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
+  } else {
+    document.querySelector("h1").innerHTML = "That's A Draw.. 🤷"
+  }
+
+  document.querySelector("h2").innerHTML = "Want To Try Agian?"
+}
